e9e597c097fb56f39aa86e6461d353f08acf5784
Tavlin <hphemmer@aol.com>
2018-05-21 15:09:25 +0200

    added adaptive stepsize

---
+++ NUMP/adaptive_stepsize.C
@@ -0,0 +1,137 @@
+// solve system of ODEs
+// \vec{\dot{y}}(t) = \vec{f}(\vec{y}(t),t) ,
+// initial conditions
+// \vec{y}(t=0) = \vec{y}_0 ,
+// anharmonic oscillator, potential
+// V(x) = m \alpha x^n ,
+// adaptive stepsize
+// **********
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+// **********
+// physics parameters and functions
+// **********
+// anharmonic oscillator, V(x) = m \alpha x^n,
+// y = (x , v)
+// f = (v , -\alpha n x^n-1)
+const int N = 2; // number of components of \vec{y} and \vec{f}
+// const int n = 2;
+// const double alpha = 0.5;
+const int n = 20;
+const double alpha = 1.0;
+double y_0[N] = { 1.0 , 0.0 }; // initial conditions
+// function computing f(y(t),t) * tau
+void f_times_tau(double *y_t, double t, double *f_times_tau_, double tau){
+
+  if(N != 2){
+    fprintf(stderr, "Error: N != 2!\n");
+    exit(EXIT_FAILURE);
+  }
+
+  f_times_tau_[0] = y_t[1] * tau;
+  f_times_tau_[1] = -alpha * ((double)n) * pow(y_t[0], ((double)(n-1))) * tau;
+}
+// **********
+// RK parameters
+// **********
+const int order = 2;
+
+
+// maximum number of steps
+const int num_steps_max = 10000;
+// compute trajectory for 0 <= t <= t_max
+const double t_max = 10.0;
+// maximum tolerable error
+const double delta_abs_max = 0.001;
+double tau = 1.0; // initial step size
+// **********
+double t[num_steps_max+1]; // discretized time
+double y[num_steps_max+1][N]; // discretized trajectories
+// **********
+
+// RK step (2nd order), step size tau
+void RK_step(double *y_t, double t, double *y_t_plus_tau, double tau){
+  int i1;
+  // *****
+  // k1 = f(y(t),t) * tau
+  double k1[N];
+  f_times_tau(y_t, t, k1, tau);
+  // *****
+  // k2 = f(y(t)+(1/2)*k1 , t+(1/2)*tau) * tau
+  double y_[N];
+  for(i1 = 0; i1 < N; i1++){
+    y_[i1] = y_t[i1] + 0.5*k1[i1];
+  }
+  double k2[N];
+  f_times_tau(y_, t + 0.5*tau, k2, tau);
+  // *****
+  for(i1 = 0; i1 < N; i1++){
+    y_t_plus_tau[i1] = y_t[i1] + k2[i1];
+  }
+}
+
+// **********
+int main(int argc, char **argv){
+  double d1;
+  int i1, i2;
+
+  // *****
+  // initialize trajectories with initial conditions
+  t[0] = 0.0;
+  for(i1 = 0; i1 < N; i1++)
+  y[0][i1] = y_0[i1];
+
+  // *****
+  // RK steps
+  for(i1 = 0; i1 < num_steps_max; i1++){
+    if(t[i1] >= t_max) break;
+
+    // *****
+    double y_tau[N], y_tmp[N], y_2_x_tau_over_2[N];
+    // y(t) --> \tau y_{\tau}(t+\tau)
+    RK_step(y[i1], t[i1], y_tau, tau);
+    // y(t) --> \tau/2 --> \tau_2 y_{2 * \tau / 2}(t+\tau)
+    RK_step(y[i1], t[i1], y_tmp, 0.5*tau);
+    RK_step(y_tmp, t[i1]+0.5*tau, y_2_x_tau_over_2, 0.5*tau);
+    // *****
+    // estimate error
+    double delta_abs = fabs(y_2_x_tau_over_2[0] - y_tau[0]);
+    for(i2 = 1; i2 < N; i2++){
+      d1 = fabs(y_2_x_tau_over_2[i2] - y_tau[i2]);
+      if(d1 > delta_abs){
+        delta_abs = d1;
+      }
+    }
+    delta_abs /= pow(2.0, (double)order) - 1.0;
+    // *****
+    // adjust step size (do not change by more than factor 5.0).
+    d1 = 0.9 * pow(delta_abs_max / delta_abs, 1.0 / (((double)order)+1.0));
+
+    if(d1 < 0.2) d1 = 0.2;
+    if(d1 > 5.0) d1 = 5.0;
+
+    double tau_new = d1 * tau;
+    // *****
+    if(delta_abs <= delta_abs_max){
+      // accept RK step
+      for(i2 = 0; i2 < N; i2++)
+      y[i1+1][i2] = y_2_x_tau_over_2[i2];
+      t[i1+1] = t[i1] + tau;
+      tau = tau_new;
+    }
+    else{
+      // repeat RK step with smaller step size
+      tau = tau_new;
+      i1--;
+    }
+  }
+  int num_steps = i1;
+  // *****
+  // output
+  for(i1 = 0; i1 <= num_steps; i1++){
+    printf("%9.6lf %9.6lf\n", t[i1], y[i1][0]);
+  }
+  // *****
+  return EXIT_SUCCESS;
+}
--- NUMP/sheet_3_1.C
+++ NUMP/sheet_3_1.C
@@ -7,10 +7,13 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 const int N = 4; //Number of needed Dimensions
-const int num_steps = 1.e3; //Number of steps
-const double tau = 1.e-2; // Stepsize
+const int num_steps = 1.e4; //Number of steps
+const double t_max = 10.0; //max time
+const double delta_max = 1.e-9; //max error
+double tau = 1.e-3; // Start stepsize
 const double eps = 0.5; //excentricity (for ellipse between 0 and 1!)
 
+double t[num_steps+1]; //array of time
 double y[N][num_steps+1]; // array of 2D points forming the Trajectory
 double y_0[N] = {1.-eps, 0, 0, sqrt((1.+eps)/(1.-eps))};
 ////////////////////////////////////////////////////////////////////////////////
@@ -19,59 +22,108 @@ double r_3(double x, double y){
   return pow(sqrt(x*x+y*y),3.);
 }
 
+
+void RK_4th_step(double* y, double* y_t_tau, double tau, int j, int N){
+  double k1[N], k2[N], k3[N], k4[N];
+  k1[0] = y[2]*tau;
+  k1[1] = y[3]*tau;
+  k1[2] = -y[0]/r_3(y[0],y[1]) *tau;
+  k1[3] = -y[1]/r_3(y[0],y[1]) *tau;
+
+  k2[0] = (y[2]+0.5*k1[2])*tau;
+  k2[1] = (y[3]+0.5*k1[3])*tau;
+  k2[2] = -(y[0]+0.5*k1[0])/r_3((y[0]+0.5*k1[0]),(y[1]+0.5*k1[1]))*tau;
+  k2[3] = -(y[1]+0.5*k1[1])/r_3((y[0]+0.5*k1[0]),(y[1]+0.5*k1[1]))*tau;
+
+  k3[0] = (y[2]+0.5*k2[2])*tau;
+  k3[1] = (y[3]+0.5*k2[3])*tau;
+  k3[2] = -(y[0]+0.5*k2[0])/r_3((y[0]+0.5*k2[0]),(y[1]+0.5*k2[1]))*tau;
+  k3[3] = -(y[1]+0.5*k2[1])/r_3((y[0]+0.5*k2[0]),(y[1]+0.5*k2[1]))*tau;
+
+  k4[0] = (y[2]+k3[2])*tau;
+  k4[1] = (y[3]+k3[3])*tau;
+  k4[2] = -(y[0]+k3[0])/r_3((y[0]+k3[0]),(y[1]+k3[1]))*tau;
+  k4[3] = -(y[1]+k3[1])/r_3((y[0]+k3[0]),(y[1]+k3[1]))*tau;
+
+  for (j = 0; j < N; j++) {
+    y_t_tau[j] = y[j] + 1./6.*(k1[j]+2*k2[j]+2*k3[j]+k4[j]);
+  }
+}
+
+
+
 ////////////////////////////////////////////////////////////////////////////////
 void sheet_3_1(void){
+  double d1;
   int i,j;
 
   //Initial Conditions
-  for (i = 0; i < N; i++) {
-    y[i][0] = y_0[i];
+  for (j = 0; j < N; j++) {
+    y[j][0] = y_0[j];
   }
+  t[0] = 0.0;
 
   //RK 4th Order
-  for (i = 1; i <= num_steps; i++) {
-    double k1[N], k2[N], k3[N], k4[N];
-    k1[0] = y[2][i-1]*tau;
-    k1[1] = y[3][i-1]*tau;
-    k1[2] = -y[0][i-1]/r_3(y[0][i-1],y[1][i-1]) *tau;
-    k1[3] = -y[1][i-1]/r_3(y[0][i-1],y[1][i-1]) *tau;
-
-    k2[0] = (y[2][i-1]+0.5*k1[2])*tau;
-    k2[1] = (y[3][i-1]+0.5*k1[3])*tau;
-    k2[2] = -(y[0][i-1]+0.5*k1[0])/r_3((y[0][i-1]+0.5*k1[0]),(y[1][i-1]+0.5*k1[1]))*tau;
-    k2[3] = -(y[1][i-1]+0.5*k1[1])/r_3((y[0][i-1]+0.5*k1[0]),(y[1][i-1]+0.5*k1[1]))*tau;
-
-    k3[0] = (y[2][i-1]+0.5*k2[2])*tau;
-    k3[1] = (y[3][i-1]+0.5*k2[3])*tau;
-    k3[2] = -(y[0][i-1]+0.5*k2[0])/r_3((y[0][i-1]+0.5*k2[0]),(y[1][i-1]+0.5*k2[1]))*tau;
-    k3[3] = -(y[1][i-1]+0.5*k2[1])/r_3((y[0][i-1]+0.5*k2[0]),(y[1][i-1]+0.5*k2[1]))*tau;
-
-    k4[0] = (y[2][i-1]+k3[2])*tau;
-    k4[1] = (y[3][i-1]+k3[3])*tau;
-    k4[2] = -(y[0][i-1]+k3[0])/r_3((y[0][i-1]+k3[0]),(y[1][i-1]+k3[1]))*tau;
-    k4[3] = -(y[1][i-1]+k3[1])/r_3((y[0][i-1]+k3[0]),(y[1][i-1]+k3[1]))*tau;
-
+  for (i = 0; i < num_steps; i++) {
+    if(t[i] >= t_max) break;
+    double y_tau[N], y_tmp[N], y_2_tau[N], yt[N];
     for (j = 0; j < N; j++) {
-      y[j][i] = y[j][i-1] + 1./6.*(k1[j]+2*k2[j]+2*k3[j]+k4[j]);
+      yt[j] = y[j][i];
+    }
+    // y_tau(t+tau)
+    RK_4th_step(yt, y_tau, tau, j, N);
+
+    // y_2*tau/2(t+2*tau/2)
+    RK_4th_step(yt, y_tmp, tau/2., j, N);
+    RK_4th_step(y_tmp, y_2_tau, tau/2., j, N);
+
+    // calculate error and compare
+    double delta_abs = fabs(y_2_tau[0] - y_tau[0]);
+    for(j = 1; j < N; j++){
+      d1 = fabs(y_2_tau[j] - y_tau[j]);
+      if(d1 > delta_abs){
+        delta_abs = d1;
+      }
+    }
+    delta_abs /= (pow(2.0, 4.0) - 1.0);
+    // *****
+    // adjust step size (do not change by more than factor 5.0).
+    d1 = 0.9 * pow(delta_max / delta_abs, 1.0 / (4.0+1.0));
+
+    if(d1 < 0.2) d1 = 0.2;
+    if(d1 > 5.0) d1 = 5.0;
+
+    // change stepsize
+    double tau_new = d1 * tau;
+    // *****
+    if(delta_abs <= delta_max){
+      // accept RK step
+      for(j = 0; j < N; j++)
+      y[j][i+1] = y_2_tau[j];
+      t[i+1] = t[i] + tau;
+      tau = tau_new;
+    }
+    else{
+      // repeat RK step with smaller step size
+      tau = tau_new;
+      i--;
     }
   }
+  int num = i;
   //////////////////////////////////////////////////////////////////////////////
   // output into a file
   ofstream myfile;
   myfile.open("kepler2D.txt");
 
-  for(i = 0; i <= num_steps; i++){
-    double t = i * tau;
-    // printf("%9.6lf %9.6lf\n", t, y[0][i]);
-    myfile << y[0][i] << "\t" << y[1][i] << "\t" << t  << endl;
+  for(i = 0; i <= num; i++){
+    myfile << y[0][i] << "\t" << y[1][i] << "\t" << t[i]  << endl;
   }
   myfile.close();
 
   ofstream myfile2;
   myfile2.open("kepler.txt");
 
-  for(i = 0; i <= num_steps; i++){
-    // double t = i * tau;
+  for(i = 0; i <= num; i++){
     myfile2 << y[0][i] << "\t" << y[1][i]  << endl;
   }
   myfile2.close();
@@ -107,7 +159,7 @@ void sheet_3_1(void){
 
 
   c1->cd();
-  gpendulum2D->Draw("P0");
+  gpendulum2D->Draw("LINE");
   // t1->AddEntry(gpendulum2D, "angle #phi (t) for a pendulum", "p");
   // t1->Draw("SAME");
   c1->Update();
